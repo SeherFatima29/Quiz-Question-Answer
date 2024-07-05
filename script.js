472ede2ea4c0c971505400a811ba25e583da1cdc
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-07-05 10:48:23 +0500

    Add files via upload
    😎

---
+++ script.js
@@ -0,0 +1,217 @@
+var score=0;
+
+var question1=prompt("which country is the largest in the world ?")
+var answer1="Russia"
+if(question1 === answer1){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question2=prompt("What is a group of crows called? ?")
+var answer2="murder"
+if(question2 === answer2){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question3=prompt("How many dots appear on a pair of dice ?")
+var answer3="42"
+if(question3 === answer3){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question4=prompt("What temperature does water boil ?")
+var answer4="212"
+if(question4 === answer4){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question5=prompt("What is the world's largest ocean ?")
+var answer5=" pacific Ocean"
+if(question5 === answer5){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question6=prompt("What is the capital city of Australia ?")
+var answer6="Canberra"
+if(question6 === answer6){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question7=prompt("What is the capital city of Pakistan ?")
+var answer7="Islamabad"
+if(question7 === answer7){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+
+var question8=prompt("What is the chemical symbol for Gold ?")
+var answer8="Au"
+if(question8 === answer8){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question9=prompt("What is the tallest mountain in the world ?")
+var answer9="Mount Everest"
+if(question9 === answer9){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question10=prompt("In what year was the first iPhone released ?")
+var answer10="29 june, 2007"
+if(question10 === answer10){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question11=prompt("What's the national flower of Japan ?")
+var answer11="Cherry blossoms"
+if(question11 === answer11){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question12=prompt("23 + 2 ?")
+var answer12="25"
+if(question12 === answer12){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question13=prompt("What's the full form of GK ?")
+var answer13="general knowledge"
+if(question13 === answer13){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question14=prompt(" 3 * 5?")
+var answer14="15"
+if(question14 === answer14){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question15=prompt("Relative to the internet, what does “URL” stand for?")
+var answer15="Uniform Resource Locator"
+if(question15 === answer15){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question16=prompt("5 + 5 ?")
+var answer16="10"
+if(question16 === answer16){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question17=prompt("15 + 5 ?")
+var answer17="20"
+if(question17 === answer17){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question18=prompt("1 + 1 ?")
+var answer18="2"
+if(question18 === answer18){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+
+var question19=prompt("12 + 1 ?")
+var answer19="13"
+if(question19 === answer19){
+alert("Correct !")
+score++
+}
+// else {
+//     alert("Loser 👎")
+// }
+
+var question20=prompt("100 + 100 ?")
+var answer20="200"
+if(question20 === answer20){
+alert("Correct !")
+score++
+}
+else {
+    alert("Loser 👎")
+}
+
+
+
+alert("your score is" + score)
